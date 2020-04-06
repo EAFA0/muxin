a42b652b2482eb324e0d6f7b0cc1c9d5aba35dca
EAFA0 <TobiichiOrigami.d@gamil.com>
2020-04-06 14:32:13 +0800

    dataset 的增删改查 view 完成

--- muxin/dataset/views.py
+++ muxin/dataset/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from rest_framework import status, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .serializers import *
+from .models import *
 
 # Create your views here.
+
+class DataSetView(generics.ListCreateAPIView,
+                  generics.RetrieveUpdateAPIView):
+
+    queryset = DataSet.objects.all()
+    serializer_class = DataSetSerializer
