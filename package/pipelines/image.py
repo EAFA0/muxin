d6567988ef400bcdadcc0bd0673980c5be611a19
EAFA0 <TobiichiOrigami.d@gmail.com>
2020-05-25 14:21:39 +0800

    package pipelines ImagesPipeline删除无用代码

--- package/pipelines/image.py
+++ package/pipelines/image.py
@@ -3,7 +3,5 @@ from scrapy.pipelines.images import ImagesPipeline as BaseImagesPipeline
 
 class ImagesPipeline(BaseImagesPipeline):
 
-    def media_downloaded(self, response, request, info):
-        # 在此提取已下载图片的信息
-        result = super().media_downloaded(response, request, info)
-        return result
+    # 暂时没有要处理的, 获取图片数据大小是个伪需求
+    pass
