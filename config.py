20a8c4afef7579707cb96a5b0f928d41699e53cc
EAFA0 <TobiichiOrigami.d@gmail.com>
2020-06-10 02:09:15 +0800

    新增 _django_host 字段, 删除重复声明

--- config.py
+++ config.py
@@ -2,31 +2,24 @@ import yaml
 
 
 class Config:
-    DJANGO_PORT = None
-    SCRAPYD_PORT = None
 
-    MYSQL_HOST = None
-    MYSQL_PORT = None
-    MYSQL_USER = None
-    MYSQL_PASSWORD = None
-    MYSQL_DATABASE = None
-
-    SCRAPYD_SERVER = None
-    DATASET_SERVER = None
-
-    instance = None
+    _instance = None
+    _django_host = "http://localhost"
 
     def __new__(cls, *arg, **kwargs):
-        if cls.instance:
-            return cls.instance
+        if cls._instance:
+            return cls._instance
         
         with open('./config.yaml') as config_file:
             config = yaml.safe_load(config_file)
 
         cls.DJANGO_PORT = config['django']['server_port']
         cls.SCRAPYD_PORT = config['scrapyd']['http_port']
-        cls.DATASET_SERVER = "http://localhost:{}/dataset".format(cls.DJANGO_PORT)
-        cls.SCRAPYD_SERVER = "http://localhost:{}".format(cls.SCRAPYD_PORT)
+        cls.SCRAPYD_SERVER = f"{cls._django_host}:{cls.SCRAPYD_PORT}"
+
+        cls.DJANGO_SERVER = f"{cls._django_host}:{cls.DJANGO_PORT}"
+        cls.DATASET_SERVER = f"{cls.DJANGO_SERVER}/dataset"
+        cls.SPIDER_TASK_SERVER = f"{cls.DJANGO_SERVER}/task/{{}}/schedule"
 
         mysql_config = config['mysql']
         cls.MYSQL_HOST = mysql_config['host']
@@ -35,8 +28,8 @@ class Config:
         cls.MYSQL_PASSWORD = mysql_config['password']
         cls.MYSQL_DATABASE = mysql_config['database']
 
-        cls.instance = object.__new__(cls, *arg, **kwargs)
-        return cls.instance
+        cls._instance = object.__new__(cls, *arg, **kwargs)
+        return cls._instance
 
 def get_config():
     return Config()
